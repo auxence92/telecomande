65719fcdf4e78dc97e7b37a7363de3be10b5bb4b
auxence92 <118936828+auxence92@users.noreply.github.com>
2022-12-14 11:59:33 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+radio.setGroup(1)
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        radio.sendNumber(1)
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        radio.sendNumber(2)
+    }
 })
