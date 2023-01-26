7d6a26eac1604425b36abf2cf3afaffb1cb99770
auxence92 <118936828+auxence92@users.noreply.github.com>
2023-01-26 11:59:50 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,13 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (0 == receivedNumber) {
         basic.showString("droite ou gauche")
+        message = 1
     }
 })
-input.onGesture(Gesture.TiltRight, function () {
-    radio.sendValue("droite", 0)
-})
-input.onGesture(Gesture.TiltLeft, function () {
-    radio.sendValue("gauche", 0)
-})
+let message = 0
 radio.setGroup(1)
+message = 0
+basic.showString("")
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         radio.sendNumber(1)
@@ -19,5 +17,18 @@ basic.forever(function () {
     }
     if (input.isGesture(Gesture.Shake)) {
         radio.sendNumber(3)
+    }
+})
+basic.forever(function () {
+    if (1 == message) {
+        while (false == input.isGesture(Gesture.TiltRight) || false == input.isGesture(Gesture.TiltLeft)) {
+            if (input.isGesture(Gesture.TiltRight)) {
+                radio.sendValue("droite", 0)
+            }
+            if (input.isGesture(Gesture.TiltLeft)) {
+                radio.sendValue("gauche", 0)
+            }
+        }
+        message = 0
     }
 })
